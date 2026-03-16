59d981a4f4c1a045f04bf3cdffaa13243824ff9b
SanderNKL <sander.lastad@gmail.com>
2026-03-16 21:54:41 +0100

    cleaned up script

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-import { renderPage } from "./modules/render/renderPage.js"
 import { typeEvent } from "./modules/typeEvent.js"
-import { pickWord } from "./modules/pickWord.js"
+import { startGame } from "./modules/startGame.js"
 
-let word = pickWord()
-console.log(word)
-// RENDER ROWS
-renderPage()
-typeEvent(word)
+
+typeEvent()
+startGame()
