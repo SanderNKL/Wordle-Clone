05bd9a9d1d7a647f47cde7816db3fd10c1587189
SanderNKL <sander.lastad@gmail.com>
2026-03-16 21:54:58 +0100

    pickWord now also sets the current word

--- modules/pickWord.js
+++ modules/pickWord.js
@@ -1,7 +1,8 @@
 import { validWords } from "./rules/validWords.js";
+import { setCurrWord } from "./rules/currentWord.js";
 
 
 export const pickWord = () => {
     const number = Math.floor(Math.random() * validWords.length - 1)
-    return validWords[number]
+    setCurrWord(validWords[number])
 }
